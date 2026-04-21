d0820524971d83fc4f1ae304d713d5876d508791
meh <meh@schizofreni.co>
2026-04-21 23:41:38 +0200

    slop: fix a double free and some missing metadata

--- tools/server/server-context.cpp
+++ tools/server/server-context.cpp
@@ -739,6 +739,9 @@ private:
     bool sleeping = false;
 
     void destroy() {
+        // Note: sleeping is set to true by handle_sleeping_state after destroy().
+        // unload_current_model() must also set sleeping = true to prevent
+        // the destructor from calling destroy() again (double free).
         llama_init.reset();
 
         ctx = nullptr;
@@ -1223,6 +1226,7 @@ private:
         }
 
         destroy();
+        sleeping = true;  // prevent destructor from calling destroy() again
         SRV_INF("%s", "model unloaded\n");
     }
 
